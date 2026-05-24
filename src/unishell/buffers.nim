6b11f9a0d497daff2b744d355f2f4574552125ae
Carmelo Maita <carmeloaugustomaitaorlando@gmail.com>
2026-05-24 00:18:11 -0400

    Finished buffers.nim and rcutable.nim

--- src/unishell/buffers.nim
+++ src/unishell/buffers.nim
@@ -6,15 +6,45 @@
   shared/dynamic/runtime-loaded libraries.
 
   It uses Nim's native strings because they behave like buffers of their own,
-  and are capable of holding binary data, such as files, within them; which
-  enables the `Buffer` object to be a buffer of buffers.
+  and are capable of holding binary data, such as files and pointers, within
+  them; which enables the `Buffer` object to be a buffer of buffers. This also
+  means that the type `cstring` is avoided on purpose to prevent truncation
+  on null bytes (`\0`), which are frequent in binary data.
+
+  *THIS OBJECT IS INTENDED TO BE READ-ONLY*, if you need to modify it;
+  convert it to a `seq[string]` and use it to create a new object.
+
+  *THIS OBJECT IS INTENDED TO BE USED IN A SINGLE-THREADED ENVIRONMENT*, if
+  you are working in a multi-threaded environment, make sure that the logic
+  that handles the object is single-threaded or is wrapped inside a lock/mutex.
+
+  Operators, iterators and procedures are provided to give the user the
+  necessary calls to be able to handle the `Buffer` and `BufferElementView` as
+  if they were a simple `seq[string]`. The following procedures enable the use
+  of:
+
+  ```nim
+  var x: Buffer = createBuffer(@["These", "are", "some", "strings"])
+
+  # Use of `in` and `$` operators
+  for i in x:
+    echo $i
+
+  # Use of the `[]` operator and comparison to `string` types
+  if x[0] == "This":
+    echo $x[0], $x[3], $x[1], $x[3] # "These strings are strings"
+
+  # Conversion to `seq[string]`
+  var y: seq[string] = toSeq(x)
+  assert x == y # True
+  ```
 ]##
 type
   Buffer* = object
     data: pointer                     ## Single block of raw concatenated string data
-    cap: uint                         ## Length of the `data` field
-    offsets: ptr UncheckedArray[uint] ## Starting offset of each string
-    sizes: ptr UncheckedArray[uint]   ## Length of each string
+    cap: int                          ## Length of the `data` field
+    offsets: ptr UncheckedArray[uint] ## Offsets to each string
+    sizes: ptr UncheckedArray[int]    ## Length of each string
     len: int                          ## Number of strings packed inside
   BufferElementView* = object
     data: ptr UncheckedArray[char]
@@ -38,13 +68,16 @@ proc freeBuffer(buf: var Buffer) =
   buf.offsets = nil
   buf.data = nil
   buf.len = 0
-  buf.cap = 0'u
+  buf.cap = 0
 
 proc `=destroy`*(buf: var Buffer) =
-  # Self-explanatory
   buf.freeBuffer()
 
 proc `=copy`*(dest: var Buffer; src: Buffer) =
+  # Auxiliary variables for the sizes to allocate and its pointers
+  var
+    sizeOfSizes: int = src.len * sizeof(int) # size of `sizes`
+    sizeOfOffsets: int = src.len * sizeof(uint) # size of `offsets`
   if dest.data != src.data:
     #[
       This case is for performing the actual copy.
@@ -59,18 +92,20 @@ proc `=copy`*(dest: var Buffer; src: Buffer) =
       content is performed
     ]#
     if src.len > 0:
-      dest.sizes = cast[ptr UncheckedArray[uint]](alloc(uint(src.len * sizeof(uint))))
-      dest.offsets = cast[ptr UncheckedArray[uint]](alloc(uint(src.len * sizeof(uint))))
-      copyMem(dest.sizes, src.sizes, src.len * sizeof(uint))
-      copyMem(dest.offsets, src.offsets, src.len * sizeof(uint))
+      dest.sizes = cast[ptr UncheckedArray[int]](alloc(sizeOfSizes))
+      dest.offsets = cast[ptr UncheckedArray[uint]](alloc(sizeOfOffsets))
+      copyMem(dest.sizes, src.sizes, sizeOfSizes)
+      copyMem(dest.offsets, src.offsets, sizeOfOffsets)
     #[
       While the following check may seem redundant, it covers the case in which
       the buffer only has empty strings (`""`). In such case, the copy of the
       data is not performed, and its value remains as `nil`
     ]#
-    if src.cap > 0'u:
+    if src.cap > 0:
       dest.data = alloc(src.cap)
-      copyMem(dest.data, src.data, src.cap.int)
+      copyMem(dest.data, src.data, src.cap)
+    else:
+      dest.data = nil
   else:
     # This case is for self-assigments.
     # They should be ignored
@@ -82,15 +117,7 @@ proc `=wasMoved`*(buffer: var Buffer) =
   buffer.offsets = nil
   buffer.data = nil
   buffer.len = 0
-  buffer.cap = 0'u
-
-proc freeBufferElementView(view: var BufferElementView) =
-  # Resets the view without destroying the data
-  view.data = nil
-  view.len = 0
-
-proc `=destroy`*(view: var BufferElementView) =
-  view.freeBufferElementView()
+  buffer.cap = 0
 
 # =============================================================================
 # INTERNAL HELPERS
@@ -112,11 +139,8 @@ proc getView(buffer: Buffer, index: int): BufferElementView {.inline.} =
     # empty.
     result.data = nil
     if result.len > 0:
-      # Gets the pointer to the UncheckedArray from:
-      # 1. Casting to an `uint` the pointer to the data
-      # 2. Adding to the pointer the offset of the data
-      # 3. Casting the result to a pointer
-      # 4. Casting the pointer to an UncheckedArray of characters
+      # Gets the pointer to the UncheckedArray from the sum of the base
+      # pointer of `data` and the `offset` fields
       result.data = cast[ptr UncheckedArray[char]](
         cast[pointer](
           cast[uint](buffer.data) + buffer.offsets[index]
@@ -125,65 +149,142 @@ proc getView(buffer: Buffer, index: int): BufferElementView {.inline.} =
   else:
     raise newException(IndexDefect, "Index out of bounds")
 
-proc `==`(view: BufferElementView; str: string): bool {.inline.} =
-  # This procedure isn't exported to not pollute the namespace
-  if view.len != str.len: return false
-  if view.len == 0: return true
-  return equalMem(view.data, unsafeAddr str[0], view.len)
-
-proc `$`(view: BufferElementView): string =
-  # This procedure isn't exported to not pollute the namespace
-  if view.len == 0 or view.data == nil: return ""
-  result = newString(view.len)
-  copyMem(addr result[0], view.data, view.len)
-
 # =============================================================================
-# PUBLIC API
+# CONSTRUCTOR (PUBLIC API)
 # =============================================================================
-
-proc len*(buf: Buffer): int {.inline.} =
-  ## Returns the number of strings contained within the buffer.
-  result = buf.len
 
 proc createBuffer*(strings: seq[string]): Buffer =
-  ## Creates a new `Buffer` object from the provided sequence of strings.
+  ##[
+    Creates a new `Buffer` object from the provided sequence of strings. Example:
+
+    ```nim
+    var
+      x: seq[string] = @["some", "strings"]
+      y: Buffer = createBuffer(x)
+    ```
+  ]##
   result.len = strings.len()
+  # Auxiliary variables for the sizes to allocate and its pointers
+  var
+    sizeOfSizes: int = result.len * sizeof(int) # size of `sizes`
+    sizeOfOffsets: int = result.len * sizeof(uint) # size of `offsets`
+    sizeOfData: int = 0                            # size of `data`
+    dataAddress: uint = 0                          # pointer to `data`
+    elementOffset: uint = 0                        # offset of an element in `data`
+    elementAddress: uint = 0                       # pointer to an element in `data`
   # Checks whether the sequence is empty or not
   if result.len != 0:
     # This is the case for a sequence that isn't empty
-    let sizeOfMetadata = uint(result.len * sizeof(uint))
-    result.sizes = cast[ptr UncheckedArray[uint]](alloc0(sizeOfMetadata))
-    result.offsets = cast[ptr UncheckedArray[uint]](alloc0(sizeOfMetadata))
+    # Allocates the memory for the metadata
+    result.sizes = cast[ptr UncheckedArray[int]](alloc0(sizeOfSizes))
+    result.offsets = cast[ptr UncheckedArray[uint]](alloc0(sizeOfOffsets))
 
-    var totalDataSize: uint = 0'u
+    # Populates the `sizes` array and gets the `sizeOfData`
     for index, element in pairs(strings):
-      result.sizes[index] = element.len().uint
-      result.offsets[index] = totalDataSize
-      totalDataSize += element.len().uint
+      result.sizes[index] = element.len()
+      sizeOfData += element.len()
 
-    if totalDataSize > 0'u:
-      result.data = alloc0(totalDataSize)
-      result.cap = totalDataSize
+    # Checks that the input isn't composed of empty strings (`""`),
+    # allocates the memory that will contain the data and sets the `cap` field
+    # to it's size
+    if sizeOfData > 0:
+      result.data = alloc0(sizeOfData)
+      result.cap = sizeOfData
     
-      let baseAddress = cast[uint](result.data)
+      # Gets the pointer to data and populates it
+      dataAddress = cast[uint](result.data) # Base pointer to the `data` field
       for index, element in pairs(strings):
+        #[
+          The address of the element is obtained after adding to the base
+          pointer of the `data` field, the current offset of the current
+          element, which starts at 0 and increments by the length (in bytes)
+          of the element after each iteration of the cycle.
+
+          The offset of empty strings is left to 0 given that on
+          extraction by the `getBufferElementView` procedure, the length
+          of the data will be read first, and based on that the pointer
+          to it will be modified from `nil` to the actual data.
+        ]#
         if element.len > 0:
-          let dstPtr = cast[pointer](baseAddress + result.offsets[index])
-          copyMem(dstPtr, unsafeAddr element[0], element.len)
+          result.offsets[index] = elementOffset
+          elementAddress = dataAddress + elementOffset
+          copyMem(cast[pointer](elementAddress), addr element[0], element.len)
+        else:
+          result.offsets[index] = 0
+        elementOffset += (result.sizes[index]).uint
+    else:
+      result.data = nil
+      result.cap = 0
   else:
     # This is the case for an empty sequence.
     # The buffer should be created in it's default state
     discard
 
 # =============================================================================
-# BASIC ITERATORS AND OPERATORS
+# OPERATORS, ITERATORS AND PROCEDURES (PUBLIC API)
 # =============================================================================
 
+proc `==`*(view: BufferElementView; str: string): bool {.inline.} =
+  ##[
+    Checks if the contained string inside a `BufferElementView` is equal to the
+    provided string. Example:
+
+    ```nim
+    var
+      x: Buffer = createBuffer(@["hello"])
+      y: string = "hello"
+
+    if x[0] == y: echo "It works!"
+    ```
+  ]##
+  #[
+    To perform this check:
+
+    1. Checks that both strings have different sizes: if true, returns false,
+       otherwise we are left to check for cases where the strings are
+       equal-sized
+    2. Checks that both strings are empty (`len == 0`): if true, returns true,
+       otherwise we are left to check for cases where the strings are not
+       empty and are equal-sized
+    3. Checks that both strings are equal by comparing their raw memory
+  ]#
+  if view.len != str.len:
+    return false
+  if (view.len == 0) and (str.len == 0):
+    return true
+  return equalMem(view.data, addr str[0], view.len)
+
+proc `$`*(view: BufferElementView): string =
+  ##[
+    Converts a `BufferElementView` into a string. Example:
+
+    ```nim
+    var x: Buffer = createBuffer(@["Hello"])
+
+    if x[0] is BufferElementView:
+      echo $x[0]
+    ```
+  ]##
+  if view.len == 0 or view.data == nil: return ""
+  result = newString(view.len)
+  copyMem(addr result[0], view.data, view.len)
+
 iterator items*(buffer: Buffer): BufferElementView =
+  ##[
+    Iterates over a `Buffer` and returns a view for each contained string.
+
+    It is necessary for the `find` procedure and the `in` operator.
+  ]##
   for i in 0 ..< buffer.len:
     yield buffer.getView(i)
 
 proc find*(buffer: Buffer, item: string): int {.inline.} =
+  ##[
+    Iterates over a `Buffer` object and returns the first index that contains
+    the provided string.
+
+    It is necessary for the `contains` procedure and the `in` operator.
+  ]##
   result = 0
   for view in buffer:
     if view == item:
@@ -192,7 +293,46 @@ proc find*(buffer: Buffer, item: string): int {.inline.} =
   return -1
 
 proc contains*(buffer: Buffer, item: string): bool {.inline.} =
+  ##[
+    Iterates over a `Buffer` object and returns `true` if the provided
+    string is inside said `Buffer`.
+    
+    It is necessary for the `in` operator.
+  ]##
   find(buffer, item) >= 0
 
-proc `[]`*(buffer: Buffer, index: int): string =
-  result = $(buffer.getView(index))
+proc len*(buf: Buffer): int {.inline.} =
+  ##[
+    Returns the number of strings contained within the buffer. Example:
+
+    ```nim
+    var x: Buffer = createBuffer(@["1", "2", "3"])
+    echo $x.len() # "3"
+    ```
+  ]##
+  result = buf.len
+
+proc `[]`*(buffer: Buffer, index: int): BufferElementView =
+  ##[
+    Returns a view to the string contained within the provided index. Example:
+
+    ```nim
+    var x: Buffer = createBuffer(@["I am a string!"])
+    echo $x[0] # "I am a string"
+    ```
+  ]##
+  result = buffer.getView(index)
+
+proc toSeq*(buffer: Buffer): seq[string] =
+  ##[
+    Creates a new `seq[string]` out of the provided `Buffer`. Example:
+
+    ```nim
+    var
+      x: Buffer = createBuffer(@["Hello", "world"])
+      y: seq[string] = x.toSeq()
+    ```
+  ]##
+  result = newSeqOfCap[string](buffer.len)
+  for element in buffer:
+    result.add($element)
--- src/unishell/rcutable.nim
+++ src/unishell/rcutable.nim
@@ -1,69 +1,141 @@
+##[
+  This module provides a concurrent hash table based on RCU, that relies
+  on Nim's ARC/ORC/Atomic ARC reference counting to deallocate the old
+  data, called `RcuTable`.
+
+  The `RcuTable` object uses an array that holds the references to two hash
+  tables, an atomic integer that represents the index of the active table and
+  a single lock that is only used by writers to avoid clashing with each other.
+  This means that one table should be active and contain actual information,
+  while the other, the inactive one, will be reserved to, on writes, perform a
+  copy of the current active table, modify it, and become the active one. The
+  specific behaviour is the following:
+
+  - Writers: copy the contents of the active table on the inactive table, modify
+    the copy, swap the value of the active index to become the active table,
+    and lastly create a new hash table, which reference will now occupy the
+    inactive slot.
+  - Readers: get the reference of the active table, which increments its counter
+    in Nim's memory manager, and perform the desired read; this prevents the
+    deallocation of the table while a write is being performed or has already
+    happened, as the table will only be deallocated after all readers have
+    finished and the counter for the reference reaches zero.
+
+  *THE `RcuTable` OBJECT IS NOT READY TO HANDLE COPIES, SO AVOID DUPLICATING IT*
+]##
 import std/[
   tables,
   atomics,
-  locks
+  locks,
+  os
 ]
 type
   RcuTable*[K, V] = object
+    ##[
+      The base object that holds the array with the references to the hash
+      tables, the atomic integer that holds the active index of the array,
+      and the lock that will be used by the writers to prevent clashing with
+      each other.
+
+      Use `RcuTableRef` instead.
+    ]##
     slots: array[2, TableRef[K, V]]
     activeSlot: Atomic[int]
     writerLock: Lock
   RcuTableRef*[K, V] = ref RcuTable[K, V]
+    ##[
+      A reference to a `RcuTable` object and also the intended object to use
+      in this module.
+    ]##
 
 proc newRcuTable*[K, V](initialsize = defaultInitialSize): RcuTableRef[K, V] =
+  ##[
+    Creates a new `RcuTable` and returns a reference to it. Example:
+
+    ```nim
+    var x: RcuTableRef = newRcuTable[int, string]()
+    # The first type is the key, and the second one the value
+    ```
+  ]##
   new(result)
   result.slots[0] = newTable[K, V](initialSize)
   result.slots[1] = newTable[K, V](initialSize)
-  result.activeSlot.store(uint(0b00))
+  result.activeSlot.store(0b00)
   result.writerLock.initLock()
 
-proc getActiveSlot*(table: RcuTableRef[K, V]): int =
+proc getActiveSlot*[K, V](table: RcuTableRef[K, V]): int {.inline.} =
+  ## Returns the index of the active table
   result = table.activeSlot.load(moAcquire)
 
-proc getInactiveSlot*(table: RcuTableRef[K, V]): int =
+proc getInactiveSlot*[K, V](table: RcuTableRef[K, V]): int {.inline.} =
+  ## Returns the index of the inactive table
   result = (table.activeSlot.load(moAcquire) xor 0b01)
 
-proc setActiveSlot*(table: RcuTableRef[K, V], newValue: int) =
+proc setActiveSlot*[K, V](table: RcuTableRef[K, V], newValue: int) {.inline.} =
+  ## Sets the index of the active table
   table.activeSlot.store(newValue, moRelease)
 
-proc commit*[K, V](table: RcuTableRef[K, V]) =
-  table.setActiveSlot(table.getInactiveSlot)
-  table.clear(table.getInactiveSlot)
-
-proc len*[K, V](table: RcuTableRef[K, V], slot: int): int =
-  var targetTable: TableRef[K, V] = table.slots[slot]
+proc len*[K, V](table: RcuTableRef[K, V]): int =
+  ## Returns the number of elements inside the selected table
+  var targetTable: TableRef[K, V] = table.slots[table.getActiveSlot()]
   result = targetTable.len()
 
-proc `[]`*[K, V](table: RcuTableRef[K, V], slot: int, key: K): var V =
-  var targetTable: TableRef[K, V] = table.slots[slot]
+proc `[]`*[K, V](table: RcuTableRef[K, V], key: K): V =
+  ## Returns the value of the given key of the selected table
+  var targetTable: TableRef[K, V] = table.slots[table.getActiveSlot()]
   result = targetTable[key]
 
-proc `[]=`*[K, V](table: RcuTableRef[K, V], slot: int, key: K, value: V) =
+proc `[]=`*[K, V](table: RcuTableRef[K, V], slot: int, key: K, value: sink V) =
+  ## Sets the value of the given key of the selected table
   var targetTable: TableRef[K, V] = table.slots[slot]
   targetTable[key] = value
 
-proc hasKey*[K, V](table: RcuTableRef[K, V], slot: int, key: K): bool =
-  var targetTable: TableRef[K, V] = table.slots[slot]
+proc hasKey*[K, V](table: RcuTableRef[K, V], key: K): bool =
+  ## Checks if the key is present in the selected table
+  var targetTable: TableRef[K, V] = table.slots[table.getActiveSlot()]
   result = targetTable.hasKey(key)
 
-proc contains*[K, V](table: RcuTableRef[K, V], slot: int, key: K): bool =
-  result = table.hasKey(slot, key)
+proc contains*[K, V](table: RcuTableRef[K, V], key: K): bool {.inline.} =
+  ## Alias for `hasKey`
+  result = table.hasKey(key)
 
 proc del*[K, V](table: RcuTableRef[K, V], slot: int, key: K) =
+  ## Deletes the given key of the selected table
   var targetTable: TableRef[K, V] = table.slots[slot]
   targetTable.del(key)
 
 proc clear*[K, V](table: RcuTableRef[K, V], slot: int, initialSize = defaultInitialSize) =
+  ## Replaces the (reference to the) selected table with a new one
   table.slots[slot] = newTable[K, V](initialSize)
+
+proc commit*[K, V](table: RcuTableRef[K, V]) =
+  ## Swaps the active table, and calls `clear` on the inactive one
+  table.setActiveSlot(table.getInactiveSlot)
+  table.clear(table.getInactiveSlot)
 
 # Modifications go to inactive slot and it is only activated once 'commit' is called
 template modify*(table: RcuTableRef, body: untyped) =
+  ##[
+    This template is the intended way of writing modifications to the
+    table, as it automatically handles the lock, copy of the data,  and call
+    `commit` after finishing. It also injects a variable called `slot` which
+    should be used as the `slot` parameter of all procedures that accept it.
+    Example:
+
+    ```nim
+    var x: RcuTableRef = newRcuTable[int, string]
+    x.modify:
+      x[slot, 0] = "Hello "
+      x[slot, 1] = "World!"
+    ```
+  ]##
   withLock(table.writerLock):
+    let
+      activeSlot: int = table.getActiveSlot()
+      inactiveSlot: int = table.getInactiveSlot()
+      slot {.inject.} = inactiveSlot
+    (table.slots[inactiveSlot])[] = (table.slots[activeSlot])[]
     try:
-      var
-        activeSlot: int = table.getActiveSlot()
-        inactiveSlot: int = activeSlot xor 0b01
-      (table.slots[inactiveSlot])[] = (table.slots[activeSlot])[]
       body
     finally:
       table.commit()
