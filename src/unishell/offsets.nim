e8963f4b699e4f4019f41e8b55c936c936827ed2
Carmelo Maita <carmeloaugustomaitaorlando@gmail.com>
2026-07-03 14:01:37 -0400

    Saved progress

--- src/unishell/offsets.nim
+++ src/unishell/offsets.nim
@@ -7,9 +7,23 @@
  `ptr UncheckedArray[int]` type.
 ]#
 
+import unishell/allocator
+
+const INTSIZE: int = sizeof(int)
+
 type
-  OffsetsObj = UncheckedArray[int]
-  Offsets = ptr OffsetsObj
-  OffsetView* = object
-    offset: Offsets
+  Offsets* = ptr UncheckedArray[int]
+  OffsetsView* = object
+    offsets: Offsets
     index: Natural
+
+proc allocator(dest: var Offsets, size: Natural, action: HostAllocatorAction) {.raises: [].} =
+  dest = cast[Offsets](hostAllocator(dest, size*INTSIZE, action))
+proc allocator(customAllocator: HostAllocator, dest: var Offsets, size: Natural, action: HostAllocatorAction) {.raises: [].} =
+  dest = cast[Offsets](customAllocator(dest, size*INTSIZE, action))
+
+proc toOffsetsView*(offsets: Offsets, len: Natural): OffsetsView {.inline, raises: [].} =
+  result.offsets = offsets
+  result.len = len
+
+proc overwriteWith*(dest: var Offsets)
--- tests/testSuiteB_Data.nim
+++ tests/testSuiteB_Data.nim
@@ -1,1 +1,94 @@
-import unishell/data
+import unishell/[
+  data,
+  allocator
+]
+
+const
+  ZEROINT:  int  = 0
+  ZEROCHAR: char = cast[char](ZEROINT)
+  TESTSTR:  string = "test"
+
+var
+  auxData: Data
+  auxView: DataView
+  auxStr:  string
+
+auxData = newData(4)
+assert (auxData != nil)
+assert (auxData[0] == ZEROCHAR)
+assert (auxData[1] == ZEROCHAR)
+assert (auxData[2] == ZEROCHAR)
+assert (auxData[3] == ZEROCHAR)
+
+auxData[0] = 't'
+auxData[1] = 'e'
+auxData[2] = 's'
+auxData[3] = 't'
+assert (auxData[0] == 't')
+assert (auxData[1] == 'e')
+assert (auxData[2] == 's')
+assert (auxData[3] == 't')
+
+auxView = auxData.toDataView(4)
+assert (auxView[0] == 't')
+assert (auxView[1] == 'e')
+assert (auxView[2] == 's')
+assert (auxView[3] == 't')
+assert (auxView == auxData)
+assert (auxData == auxView)
+assert (auxView == newData("test").toDataView(4))
+assert (auxView == "test")
+assert ("test" == auxView)
+assert (TESTSTR.toDataView() == auxView)
+assert (auxView == TESTSTR.toDataView())
+
+auxView.overwriteWith("tset")
+assert (auxView[0] == 't')
+assert (auxView[1] == 's')
+assert (auxView[2] == 'e')
+assert (auxView[3] == 't')
+
+auxView.overwriteWith("test".toDataView())
+assert (auxView[0] == 't')
+assert (auxView[1] == 'e')
+assert (auxView[2] == 's')
+assert (auxView[3] == 't')
+
+auxStr = $auxView
+assert (auxStr == "test")
+
+auxData.destroyData()
+assert (auxData == nil)
+
+auxData = newData(hostAllocator, 4)
+assert (auxData != nil)
+destroyData(hostAllocator, auxData)
+assert (auxData == nil)
+auxData = newData(hostAllocator, "test")
+assert (auxData != nil)
+
+auxView = auxData.toDataView(4)
+assert (auxView[0] == 't')
+assert (auxView[1] == 'e')
+assert (auxView[2] == 's')
+assert (auxView[3] == 't')
+
+overwriteWith(hostAllocator, auxView, "tset".toDataView())
+assert (auxView[0] == 't')
+assert (auxView[1] == 's')
+assert (auxView[2] == 'e')
+assert (auxView[3] == 't')
+overwriteWith(hostAllocator, auxView, "test")
+assert (auxView[0] == 't')
+assert (auxView[1] == 'e')
+assert (auxView[2] == 's')
+assert (auxView[3] == 't')
+
+assert ('t' in auxView)
+assert not ('a' in auxView)
+for i in auxView:
+  assert (i in "test")
+assert (auxView.len() == 4)
+
+destroyData(hostAllocator, auxData)
+assert (auxData == nil)
